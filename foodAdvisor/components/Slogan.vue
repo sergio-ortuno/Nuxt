4489f5c1ded745907cd8500f0cee126654184e6d
Sergio Ortuno <sortunoiniesta@gmail.com>
2021-04-02 15:48:42 +0200

    APIs

--- foodAdvisor/components/Slogan.vue
+++ foodAdvisor/components/Slogan.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h2 class="title is-2">FoodAdvisor, tu web de restaurantes</h2>
-    <div class="subscribe">
-      <input type="text" class="input email" placeholder="email..." />
-      <button class="button is-info">Suscribirse</button>
+    <div class="subscribe" v-if="!is_subscribe">
+      <input
+        v-model="email"
+        type="text"
+        class="input email"
+        placeholder="email..."
+      />
+      <button class="button is-info" @click="subscribe">Suscribirse</button>
+    </div>
+    <div v-else>
+      <p>Gracias! Ahora encontras cada semana una recomendacion en tu email.</p>
     </div>
   </div>
 </template>
@@ -18,3 +26,27 @@
   width: 80%;
 }
 </style>
+
+<script>
+import api from "@/services/api";
+export default {
+  data() {
+    return {
+      email: "",
+      is_subscribe: false,
+    };
+  },
+  methods: {
+    async subscribe() {
+      const payload = {
+        email: this.email,
+      };
+      const response = await api.postSubscribeUser(payload);
+      if (response.status == 201) {
+        //201: Created
+        this.is_subscribe = true;
+      }
+    },
+  },
+};
+</script>
--- foodAdvisor/services/api.js
+++ foodAdvisor/services/api.js
@@ -20,8 +20,29 @@ async function apiGet(url) {
   }
 }
 
+async function apiPost(url, payload) {
+  const config = {
+    method: 'get',
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: payload
+  }
+  const urlCall = API_URL + url
+  try {
+    const response = await axios.post(urlCall, config)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 api.getRestaurants = () => {
   return apiGet('restaurants')
+}
+api.postSubscribeUser = payload => {
+  return apiPost('users', payload)
 }
 
 export default api
